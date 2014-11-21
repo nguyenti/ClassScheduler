d0523db8066ae7fe6c07ff9c215b72f97357502b
nguyenti <tn.xc.lax@gmail.com>
2014-11-21 16:23:01 +0100

    Fix options bug and extract string resources

--- app/src/main/java/hu/ait/tiffanynguyen/classscheduler/DayListActivity.java
+++ app/src/main/java/hu/ait/tiffanynguyen/classscheduler/DayListActivity.java
@@ -89,7 +89,7 @@ public class DayListActivity extends Activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.class_list, menu);
+        getMenuInflater().inflate(R.menu.activity_day_list, menu);
         return true;
     }
 
